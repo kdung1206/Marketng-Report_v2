9f24289e32bc1fd5b5f023ac78e9264068e7281d
kdung1206 <170417078+kdung1206@users.noreply.github.com>
2026-07-03 10:26:19 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import {defineConfig} from 'vite';
 export default defineConfig(() => {
   return {
     plugins: [react(), tailwindcss()],
+    base: '/Marketng-Report_v2/',
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
